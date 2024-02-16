79efcc143b671a0ac9e3effb28f58ed5802ab05a
manuelxantony <manuelxantony@gmail.com>
2024-02-16 23:32:11 +0530

    fixed description layout

--- components/about/about.tsx
+++ components/about/about.tsx
@@ -1,13 +1,13 @@
 export default function About() {
   return (
-    <section>
-      <div className="container mx-auto">
-        <div className=" md:h-[820px] flex flex-col justify-center items-center">
+    <section className="container mx-auto">
+      <div className="md:h-[820px] flex justify-center items-center">
+        <div className="flex flex-col gap-8">
           <h1>
-            {`Hi, I'm`}
+            {`Hi, I'm `}
             <span className="font-bold uppercase">Manuel Antony</span>
           </h1>
-          <h3>writing web application</h3>
+          <h3 className="text-left max-w-xl">writing web application</h3>
         </div>
       </div>
     </section>
