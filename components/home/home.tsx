023aca7a2dbc55cfef518d03b77ce172bd1ef1e5
manuelxantony <manuelxantony@gmail.com>
2024-02-20 20:39:45 +0530

    added bg image

--- components/home/home.tsx
+++ components/home/home.tsx
@@ -4,16 +4,17 @@ import AnimatedText from '@/components/animated/animatedText';
 
 export default function Home() {
   return (
-    <section className="container mx-auto ">
-      <div className="h-screen flex justify-center items-center">
+    <section className="container mx-auto relative opacity-100">
+      <div className="h-screen w-full  stary-sky-bg"></div>
+      <div className="flex justify-center items-center absolute top-0 bottom-0 right-0 left-0">
         <div className="flex flex-col gap-8">
           <div className="flex flex-col md:flex-row gap-5">
-            <h1 className="font-thin">{`Hi, I'm `}</h1>
+            <h1 className="font-thin opacity-50">{`Hi, I'm `}</h1>
             <h1 className="font-bold">
               <AnimatedText text="Manuel Antony" />
             </h1>
           </div>
-          <p className="text-left text-[22px] tracking-wider font-thin">
+          <p className="text-left text-[22px] tracking-wider font-thin opacity-50">
             Writing applications for web in nextjs
           </p>
         </div>
