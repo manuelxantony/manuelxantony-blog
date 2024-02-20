bc65ee329c77501390e7ded18f85f39e7fb14ffc
manuelxantony <manuelxantony@gmail.com>
2024-02-20 19:28:19 +0530

    removed stars.css

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Home from '@/components/home/home';
 import About from '@/components/about/about';
-import Border from '@/components/border';
 import Projects from '@/components/projects/projects';
 
 export default function Page() {
